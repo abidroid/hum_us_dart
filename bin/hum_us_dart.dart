f0b6808e3b741872fd34c6187feb521f7d6b9e98
Abid <abidroid1@gmail.com>
2025-01-13 08:38:16 +0500

    user input

--- bin/hum_us_dart.dart
+++ bin/hum_us_dart.dart
@@ -8,132 +8,20 @@ program
 
 // entry point
 
-void main()
-{
-  // print('Hello World!');
-  // print('From Pakistan');
-  // print(5 + 3 );
-  // print('5 + 3');
-  // print( 5 > 2 );
-  // print( 5 < 2 );
-  //
-  // // variable are containers
-  // // that holds data
-  //
-  // // variable declare
-  // // datatype variableName;
-  // String name;
-  // // variable initialize
-  // name = "Ali";
-  //
-  // /*
-  // int ( number - whole number )
-  // double ( number - fraction point )
-  // num ( int, double type value )
-  // bool ( true / false )
-  // String ( alphanumeric values )
-  //  */
-  //
-  // int totalStudents;
-  // // camel Casing
-  // totalStudents = 40;
-  // print(totalStudents);
-  // totalStudents = totalStudents + 5;
-  // print(totalStudents);
-  //
-  // double gpa = 3;
-  // print(gpa);
-  //
-  // bool billPaid = false;
-  //
-  // String address = "FF#1042, Deans Trade Center Peshawar";
-  //
-  // // arithmetic + , - ( number )
-  // String cnic = "17831-83838383-3";
-  //
-  // num value = 5;
-  // print(value.runtimeType);
-  // value = 5.6;
-  // print(value.runtimeType);
-  //
-  // // constant values
-  // final double pi = 3.14;
-  // print(pi);
+import 'dart:io';
 
+void main() {
 
-  // Type inference
-  // var fatherName =  "Ali";
-  // print("My father name is $fatherName");
-  //
-  // var sscMarks = 789;
-  // print(sscMarks.runtimeType);
-  //
-  // dynamic value = "USA";
-  // value = 13;
-  // value = true;
-  // value = 3.3;
-  //
-  //
-  // // Optional value
-  // // swift language
-  // String? mangetarName;
-  // print(mangetarName);
-  //
-  // double? gpa;
-  // gpa = 3.3;
-  // print(gpa);
-  // // String interpolation
-  // print("My gpa is $gpa");
-  //
-  //
-  // // String
-  // String firstName = "Abid";
-  // String lastName = "Ali";
-  //
-  // String fName = "$firstName $lastName";
-  // print(fName);
+  String? name;
+  stdout.write("Enter your name: ");
+  name = stdin.readLineSync();
 
+  print('Welcome $name');
 
-  // print( 5 + 7 );
-  // print( 5 - 7 );
-  // print( 5 * 7 );
-  //
-  // print( 20 / 7 );
-  // print( 20 ~/ 7); // floor
-  //
-  // print( 11 % 2 );
-
-  // int n1, n2, result;
-  // n1 = 7;
-  // n2 = 5;
-  // print( "Number one = $n1");
-  // print( "Number two = $n2");
-  //
-  // result = n1 + n2;
-  // print("Sum = $result");
-
-
-  String n1 = '5';
-  String n2 = '7';
-
-  print(n1+n2);
-
-  // how to convert a String to an int
-  // int.parse( String )
-
-  int num1 = int.parse(n1);
-  int num2 = int.parse(n2);
-
-  print(num1+num2);
-
-  // how to convert a String to a double value
-  String value1 = "8.3";
-  String value2 = "5.5";
-
-  print(value1 + value2);
-  print( double.parse(value1) + double.parse(value2) );
-
-
+  String country;
+  stdout.write("Enter your country: ");
+  country = stdin.readLineSync()!;
+  print('how is your country $country');
 
 
 }
