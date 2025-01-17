85c66495808149f99e13526f2e45a42536a08d5f
Abid <abidroid1@gmail.com>
2025-01-17 20:38:09 +0500

    sum of 3 numbers

--- bin/hum_us_dart.dart
+++ bin/hum_us_dart.dart
@@ -5,40 +5,26 @@ Dart
 program
  */
 
-
 // entry point
 
 import 'dart:io';
 
 void main() {
+  String num1, num2, num3;
+  stdout.write('Enter first number : ');
+  num1 = stdin.readLineSync()!;
 
-  String? age;
-  //age = '25'; // hard coded value
+  stdout.write('Enter second number: ');
+  num2 = stdin.readLineSync()!;
 
-  stdout.write("Enter your age: ");
-  age = stdin.readLineSync();
+  stdout.write('Enter third number: ');
+  num3 = stdin.readLineSync()!;
 
-  int currentAge = int.parse(age!);
+  int n1, n2, n3;
+  n1 = int.parse(num1);
+  n2 = int.parse(num2);
+  n3 = int.parse(num3);
 
-  int ageAfter10Years = currentAge + 10;
-
-  print('After 10 years you will be $ageAfter10Years years old');
-
-
+  int result = n1 + n2 + n3;
+  print('Sum : $result');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
